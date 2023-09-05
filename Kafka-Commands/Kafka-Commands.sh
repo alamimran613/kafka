3a5f045d9593bb860292f44212d16f65800921cf
Imran Alam <112178589+alamimran613@users.noreply.github.com>
2023-09-05 15:07:54 +0530

    Update Kafka-Commands.sh

--- Kafka-Commands/Kafka-Commands.sh
+++ Kafka-Commands/Kafka-Commands.sh
@@ -1,2 +1,8 @@
 # List cluster topics with SSL port - make sure to mention your public dns here before :9093
 kafka/bin/kafka-topics.sh --bootstrap-server ec2-13-235-95-72.ap-south-1.compute.amazonaws.com:9093 --list --command-config ssl/client.properties
+
+# Produce message through key
+bin/kafka-console-producer.sh --bootstrap-server localhost:9092 --topic my-topic --property "parse.key=true" --property "key.separator=:"
+# Message producer example
+<key name>:<value>
+Ex - mykey:hello world
